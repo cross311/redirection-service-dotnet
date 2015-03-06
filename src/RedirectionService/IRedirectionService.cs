0f2f379b8a38ea7734ea099fa6338e23b35cfd54
Connor Ross <cross@mdsol.com>
2015-03-05 23:26:32 -0500

    initial requirements met

---
+++ src/RedirectionService/IRedirectionService.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectionService
+{
+    public interface IRedirectionService
+    {
+        Redirection ForTokenRedirectToLocation(ForTokenRedirectToLocationRequest forTokenRedirectToLocationRequest);
+
+        Redirection LocationToRedirectForToken(LocationToRedirectForTokenRequest locationToRedirectForTokenRequest);
+    }
+
+    public sealed class RedirectionServiceFactory
+    {
+
+        public IRedirectionService Build()
+        {
+            var redirectionRepository = new InMemoryRedirectionRepository();
+            var redirectionService = new TokenBasedRedirectionService(redirectionRepository);
+            return redirectionService;
+        }
+    }
+
+    internal sealed class TokenBasedRedirectionService : IRedirectionService
+    {
+        private readonly IRedirectionRepository _RedirectionRepository;
+
+        public TokenBasedRedirectionService(IRedirectionRepository redirectionRepository)
+        {
+            _RedirectionRepository = redirectionRepository;
+        }
+
+        public Redirection ForTokenRedirectToLocation(ForTokenRedirectToLocationRequest forTokenRedirectToLocationRequest)
+        {
+            var token = forTokenRedirectToLocationRequest.Token;
+            var location = forTokenRedirectToLocationRequest.Location;
+            var redirection = new Redirection(token, location);
+
+            redirection = _RedirectionRepository.SaveRedirection(redirection);
+
+            return redirection;
+        }
+
+        public Redirection LocationToRedirectForToken(LocationToRedirectForTokenRequest locationToRedirectForTokenRequest)
+        {
+            var token = locationToRedirectForTokenRequest.Token;
+            var redirection = _RedirectionRepository.GetRedirectionForToken(token);
+
+            return redirection;
+        }
+    }
+
+    internal interface IRedirectionRepository
+    {
+        Redirection SaveRedirection(Redirection redirection);
+        Redirection GetRedirectionForToken(string token);
+    }
+
+    internal sealed class InMemoryRedirectionRepository : IRedirectionRepository
+    {
+        private readonly ConcurrentDictionary<string, Redirection> _Database;
+
+        public InMemoryRedirectionRepository()
+        {
+            _Database = new ConcurrentDictionary<string, Redirection>();
+        }
+
+        public Redirection SaveRedirection(Redirection redirection)
+        {
+            var savedRedirection = _Database.AddOrUpdate(redirection.Token, redirection, (token, oldRedirectino) => redirection);
+
+            return savedRedirection;
+        }
+
+        public Redirection GetRedirectionForToken(string token)
+        {
+            if (!_Database.ContainsKey(token))
+                return Redirection.Empty;
+
+            var redirection = _Database[token];
+
+            return redirection;
+        }
+    }
+
+    public sealed class Redirection
+    {
+        private readonly string _Token;
+        private readonly string _Location;
+
+        public Redirection(string token, string location)
+        {
+            _Token = token;
+            _Location = location;
+        }
+
+        public string Token
+        {
+            get { return _Token; }
+        }
+
+        public string Location
+        {
+            get { return _Location; }
+        }
+
+        // NULL VALUE PATTERN
+        public readonly static Redirection Empty = new Redirection(string.Empty, string.Empty);
+    }
+
+    public sealed class ForTokenRedirectToLocationRequest
+    {
+        private readonly string _Token;
+        private readonly string _Location;
+
+        public ForTokenRedirectToLocationRequest(string token, string location)
+        {
+            _Token = token;
+            _Location = location;
+        }
+
+        public string Token
+        {
+            get { return _Token; }
+        }
+
+        public string Location
+        {
+            get { return _Location; }
+        }
+    }
+
+    public sealed class LocationToRedirectForTokenRequest
+    {
+        private readonly string _Token;
+
+        public LocationToRedirectForTokenRequest(string token)
+        {
+            _Token = token;
+        }
+
+        public string Token
+        {
+            get { return _Token; }
+        }
+    }
+}
---
+++ src/Test/RedirectionService.Test/RedirectionServiceShould.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace RedirectionService.Test
+{
+    [TestClass]
+    public class RedirectionServiceShould
+    {
+        private IRedirectionService _RedirectionService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _RedirectionService = new RedirectionServiceFactory().Build();
+        }
+
+        [TestMethod]
+        public void CreateARedirectionLocationForARedirectionToken()
+        {
+            // arrange
+            var token                             = @"test_token";
+            var location                          = @"http://www.test_token_redirection_location.com";
+
+            var forTokenRedirectToLocationRequest = new ForTokenRedirectToLocationRequest(
+                                                        token: token,
+                                                        location: location);
+
+            // act
+            var redirection = _RedirectionService.ForTokenRedirectToLocation(forTokenRedirectToLocationRequest);
+
+            //assert
+            redirection.Token.Should().Be(token);
+            redirection.Location.Should().Be(location);
+        }
+
+        [TestMethod]
+        public void ReturnRedirectionLocationForRedirectionToken()
+        {
+            //arrange
+            var token                  = @"test_token";
+            var location               = @"http://www.test_token_redirection_location.com";
+
+            var forTokenRedirectToLocationRequest = new ForTokenRedirectToLocationRequest(
+                                                        token: token,
+                                                        location: location);
+
+            _RedirectionService.ForTokenRedirectToLocation(forTokenRedirectToLocationRequest);
+
+            var locationToRedirectForTokenRequest = new LocationToRedirectForTokenRequest(
+                                                        token: token);
+
+            // act
+            var redirection = _RedirectionService.LocationToRedirectForToken(locationToRedirectForTokenRequest);
+
+            // assert
+            redirection.Token.Should().Be(token);
+            redirection.Location.Should().Be(location);
+        }
+    }
+}
